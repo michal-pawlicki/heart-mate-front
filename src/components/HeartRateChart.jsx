295785ac6982a9092dfd8b621bc9860e364ce8bb
michal-pawlicki <michalpawlicki02@gmail.com>
2023-01-14 21:44:57 +0100

    fixes

--- src/components/HeartRateChart.jsx
+++ src/components/HeartRateChart.jsx
@@ -1,7 +1,75 @@
 import React from "react";
+import { useState, useEffect, useMemo } from "react";
+import {
+  LineChart,
+  Line,
+  CartesianGrid,
+  XAxis,
+  YAxis,
+  Tooltip,
+  Label,
+} from "recharts";
+
+const Tick = () => {
+  return <div />;
+};
+
+const CustomTooltip = ({ active, payload }) => {
+  if (active && payload && payload.length) {
+    return (
+      <div className="border-2 rounded-md bg-purple-100 p-2 border-purple-600">
+        <p className="text-purple-600">{`${payload[0].value} BPM`}</p>
+      </div>
+    );
+  }
+
+  return null;
+};
 
 const HeartRateChart = (props) => {
-  return <div>HeartRateChart</div>;
+  const [[height, width], setSize] = useState([
+    window.innerHeight,
+    window.innerWidth,
+  ]);
+
+  useEffect(() => {
+    window.addEventListener("resize", (e) => {
+      setSize([e.target.innerHeight, e.target.innerWidth]);
+    });
+
+    return () => {
+      window.removeEventListener("resize", () => {});
+    };
+  }, []);
+
+  const [calcWidth, calcHeight] = useMemo(() => {
+    if (window.innerWidth > 1024) {
+      return [width / 2, height / 2];
+    }
+
+    return [width * 0.9, height / 2];
+  }, [width, height]);
+
+  return (
+    <LineChart
+      width={calcWidth}
+      height={calcHeight}
+      data={props.data.slice(0, 21)}
+      margin={{ top: 10, right: 10, bottom: 10, left: 10 }}
+    >
+      <Line
+        type="monotone"
+        dataKey="value"
+        stroke="#9333ea"
+        isAnimationActive={false}
+      />
+      <CartesianGrid stroke="#ccc" />
+      <XAxis domain={[0, 10]} reversed={true} tick={Tick} />
+
+      <YAxis domain={[30, 300]} tickCount={10}></YAxis>
+      <Tooltip content={<CustomTooltip />} />
+    </LineChart>
+  );
 };
 
 export default HeartRateChart;
--- src/pages/session.js
+++ src/pages/session.js
@@ -1,8 +1,8 @@
 import React from "react";
 import { useState, useEffect, useMemo } from "react";
-import Chart from "../components/Chart";
 import io from "socket.io-client";
 import { useSocket } from "../service/socket";
+import HeartRateChart from "../components/HeartRateChart";
 
 const Session = () => {
   const [data, setData] = useState([
@@ -17,7 +17,7 @@ const Session = () => {
 
   const [working, setWorking] = useState(false);
 
-  // const [name, setName] = useState(promps("Enter your name"));
+  const [name, setName] = useState("Change Name");
 
   const handleReset = () => {
     setData([]);
@@ -35,20 +35,20 @@ const Session = () => {
 
   const socket = useSocket();
 
+  const changeUsername = () => {
+    setName(prompt("enter your name"));
+  };
+
   useEffect(() => {
     if (working) {
-      // socket.on("updateSensorData", function (msg) {
-      //   setData((previous) => [...previous, { value: msg.value }]);
-      // });
       socket.on("pulse", (val) => {
         setData((previous) => [{ value: val }, ...previous]);
         console.log(val);
       });
-      // count += 1;
     } else {
       socket.off("pulse");
     }
-  }, [working]);
+  }, [working, socket]);
 
   const handleStart = () => {
     setWorking(true);
@@ -56,27 +56,32 @@ const Session = () => {
 
   const handleStop = () => {
     setWorking(false);
-    // const sum = data.reduce((accumulator, object) => {
-    //   return accumulator + object.salary;
-    // }, 0);
-    // fetch("192.168.1.55/add_measurement", {
-    //   method: "POST",
-    //   body: JSON.stringify({
-    //     name: { name },
-    //     date: "19-01-2023",
-    //     value: {},
-    //   }),
-    // });
+    const date = new Date();
+    fetch("192.168.1.55/add_measurement", {
+      method: "POST",
+      body: JSON.stringify({
+        name: { name },
+        date: { date },
+        value: { mean },
+      }),
+    });
   };
-
-  const color = useMemo(() => {
-    if (mean < 120) return "rgba()";
-  }, [mean]);
 
   return (
     <div className="flex flex-col lg:flex-row justify-center item-center p-10">
-      <Chart data={data} />
+      <HeartRateChart data={data} />
       <div className="flex flex-col justify-center items-center ">
+        <div className="flex">
+          <h1 className="text-2xl">{name}</h1>
+          <button
+            className="w-3 h-3 fill-purple-600 mx-2"
+            onClick={changeUsername}
+          >
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+              <path d="M373.1 24.97C401.2-3.147 446.8-3.147 474.9 24.97L487 37.09C515.1 65.21 515.1 110.8 487 138.9L289.8 336.2C281.1 344.8 270.4 351.1 258.6 354.5L158.6 383.1C150.2 385.5 141.2 383.1 135 376.1C128.9 370.8 126.5 361.8 128.9 353.4L157.5 253.4C160.9 241.6 167.2 230.9 175.8 222.2L373.1 24.97zM440.1 58.91C431.6 49.54 416.4 49.54 407 58.91L377.9 88L424 134.1L453.1 104.1C462.5 95.6 462.5 80.4 453.1 71.03L440.1 58.91zM203.7 266.6L186.9 325.1L245.4 308.3C249.4 307.2 252.9 305.1 255.8 302.2L390.1 168L344 121.9L209.8 256.2C206.9 259.1 204.8 262.6 203.7 266.6zM200 64C213.3 64 224 74.75 224 88C224 101.3 213.3 112 200 112H88C65.91 112 48 129.9 48 152V424C48 446.1 65.91 464 88 464H360C382.1 464 400 446.1 400 424V312C400 298.7 410.7 288 424 288C437.3 288 448 298.7 448 312V424C448 472.6 408.6 512 360 512H88C39.4 512 0 472.6 0 424V152C0 103.4 39.4 64 88 64H200z" />
+            </svg>
+          </button>
+        </div>
         <div className={"text-5xl md:text-6xl"}>{mean} bpm</div>
         <div>
           <button
